8dbe30026dd72365ee7914856c24fc7b52c28fb6
ono623 <1no514ki@gmail.com>
2019-10-23 05:16:22 +0000

    Some fix

--- kadai-1/index.js
+++ kadai-1/index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const name='Sushi'
+console.log(name)
+let name='Oden'
+console.log(name)
+let number = 8
+console.log(number)
+number = number+8
+console.log(number)//これを書くのに前回の課題消してしまったのですがどうしたらいいでしょう
